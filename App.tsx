fc55fc1447f9d9c093776c5f862c972bc6784fd7
Pedro Wesley <pedro.wesley@osf.digital>
2022-10-23 18:28:59 -0300

    Inclusão de font Roboto com expo google fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
-import { StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet } from "react-native";
 import { ThemeProvider } from "styled-components";
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Groups />
-    </ThemeProvider>
-  );
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  return <ThemeProvider theme={theme}>{fontsLoaded ? <Groups /> : <ActivityIndicator />}</ThemeProvider>;
 }
 
 const styles = StyleSheet.create({
