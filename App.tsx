acdb669aa69cba375b196528873547d3c9f26883
Pedro Wesley <pedro.wesley@osf.digital>
2022-10-23 18:14:17 -0300

    Criação de tema para a aplicação e tipagem de tema com DefaultTheme

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import { Groups } from "@screens/Groups";
+import theme from "@theme/index";
 import { StyleSheet } from "react-native";
+import { ThemeProvider } from "styled-components";
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
 
 const styles = StyleSheet.create({
