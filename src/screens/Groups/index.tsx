bc4c662ee76d795aa2af061fbead5d121e1f9bf3
Pedro Wesley <pedro.wesley@osf.digital>
2022-10-26 20:21:34 -0300

    instalação e utilização de pack de icons

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,11 @@
+import { Header } from "@components/Header";
 import React from "react";
 import { Container } from "./styles";
 
 export function Groups() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 }
